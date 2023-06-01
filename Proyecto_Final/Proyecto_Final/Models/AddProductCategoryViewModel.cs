847d7196a6a393e9bb7e852d456a440ce3924960
Valenciasebas4 <valenciasebastian80@gmail.com>
2023-06-01 13:35:34 -0500

    Create view products and models products and controller

---
+++ Proyecto_Final/Proyecto_Final/Models/AddProductCategoryViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace Proyecto_Final.Models
+{
+    public class AddProductCategoryViewModel
+    {
+        public Guid ProductId { get; set; }
+
+        [Display(Name = "Categoría")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public Guid CategoryId { get; set; }
+
+        public IEnumerable<SelectListItem> Categories { get; set; } 
+    }
+}
---
+++ Proyecto_Final/Proyecto_Final/Models/AddProductImageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Proyecto_Final.Models
+{
+    public class AddProductImageViewModel
+    {
+        public Guid ProductId { get; set; }
+
+        [Display(Name = "Foto")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public IFormFile ImageFile { get; set; }
+    }
+}
