1054c1eec6777bb241f0c5b424c636405e204725
Valenciasebas4 <valenciasebastian80@gmail.com>
2023-06-05 17:25:44 -0500

    delete TrainingUserController

---
+++ Proyecto_Final/Proyecto_Final/Helpers/ITrainingUserHelper.cs
@@ -0,0 +1,13 @@
+﻿
+using Proyecto_Final.Common;
+using Proyecto_Final.Models;
+
+
+
+namespace Proyecto_Final.Helpers
+{
+    public interface ITrainingUserHelper
+    {
+        Task<Response2> ProcessTrainingUserAsync(AddTrainingUserViewModel addTrainingUserViewModel);
+    }
+}
---
+++ Proyecto_Final/Proyecto_Final/Services/TrainingUserHelper.cs
@@ -0,0 +1,47 @@
+﻿
+using Proyecto_Final.DAL.Entities;
+using Proyecto_Final.DAL;
+using Proyecto_Final.Enum;
+using Proyecto_Final.Helpers;
+using Proyecto_Final.Models;
+using Proyecto_Final.Common;
+using Azure;
+
+namespace Proyecto_Final.Services
+{
+    public class TrainingUserHelper : ITrainingUserHelper
+    {
+        private readonly DataBaseContext _context;
+
+        public TrainingUserHelper(DataBaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Response2> ProcessTrainingUserAsync(AddTrainingUserViewModel addTrainingUserViewModel)
+        {
+            
+
+            TrainingUser TrainingUser = new()
+            {
+                CreatedDate = DateTime.Now,
+                User = addTrainingUserViewModel.User,
+                Training = addTrainingUserViewModel.Training,
+                ClassDate = addTrainingUserViewModel.DateClass,
+                
+
+            };
+
+         
+                
+            
+
+            _context.TrainingsUser.Add(TrainingUser);
+            await _context.SaveChangesAsync();
+            return TrainingUser;
+        }
+
+       
+
+    }
+}
