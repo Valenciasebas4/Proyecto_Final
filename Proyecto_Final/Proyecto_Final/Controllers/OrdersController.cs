ff7e49f70d58e5f8d3fd923436603bb3f3ec63b1
Valenciasebas4 <valenciasebastian80@gmail.com>
2023-06-08 08:16:22 -0500

    New OrdersController created to admin orders.

---
+++ Proyecto_Final/Proyecto_Final/Controllers/OrdersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Proyecto_Final.DAL;
+using System.Data;
+
+namespace Proyecto_Final.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class OrdersController : Controller
+    {
+        private readonly DataBaseContext _context;
+
+        public OrdersController(DataBaseContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Orders
+                .Include(s => s.User)
+                .Include(s => s.OrderDetails)
+                .ThenInclude(sd => sd.Product)
+                .ToListAsync());
+        }
+    }
+}
