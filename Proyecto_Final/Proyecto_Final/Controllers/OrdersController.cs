7c21f181913cdae0caa4ae47a723911294663ad1
Valenciasebas4 <valenciasebastian80@gmail.com>
2023-06-08 08:23:25 -0500

    Action Details in OrdersController created with partial view.

--- Proyecto_Final/Proyecto_Final/Controllers/OrdersController.cs
+++ Proyecto_Final/Proyecto_Final/Controllers/OrdersController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Proyecto_Final.DAL;
+using Proyecto_Final.DAL.Entities;
 using System.Data;
 
 namespace Proyecto_Final.Controllers
@@ -22,6 +23,23 @@ namespace Proyecto_Final.Controllers
                 .Include(s => s.OrderDetails)
                 .ThenInclude(sd => sd.Product)
                 .ToListAsync());
+        }
+
+
+        public async Task<IActionResult> Details(Guid? orderId)
+        {
+            if (orderId == null) return NotFound();
+
+            Order order = await _context.Orders
+                .Include(s => s.User)
+                .Include(s => s.OrderDetails)
+                .ThenInclude(sd => sd.Product)
+                .ThenInclude(p => p.ProductImages)
+                .FirstOrDefaultAsync(s => s.Id == orderId);
+
+            if (order == null) return NotFound();
+
+            return View(order);
         }
     }
 }
