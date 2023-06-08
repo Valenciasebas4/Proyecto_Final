f03bdae1c92d010ebb88a9b3d3153d417c9ed418
Valenciasebas4 <valenciasebastian80@gmail.com>
2023-06-08 13:31:04 -0500

    New View and Action Edit UserTraining

--- Proyecto_Final/Proyecto_Final/Controllers/UserTrainingController.cs
+++ Proyecto_Final/Proyecto_Final/Controllers/UserTrainingController.cs
@@ -74,7 +74,7 @@ namespace Proyecto_Final.Controllers
                        Problem("Entity set 'DataBaseContext.UserTrainings'  is null.");
             */
         }
-        public async Task<IActionResult> MyTrainings()
+        public async Task<IActionResult> MyTrainings() 
         {
             ViewBag.UserFullName = GetUserFullName();
 
@@ -85,16 +85,9 @@ namespace Proyecto_Final.Controllers
             return View(await _context.UserTrainings
                 .Include(o => o.User)
                 .Include(o => o.Training)
-
-                // Where(o => o.User.UserName == User.Identity.Name)
                 .ToListAsync());
             Problem("Entity set 'DataBaseContext.UserTrainings'  is null.");
-            /*
-            return _context.UserTrainings != null ?
-                        View(await _context.UserTrainings.ToListAsync()) :
-                        
-                       Problem("Entity set 'DataBaseContext.UserTrainings'  is null.");
-            */
+            
         }
 
 
@@ -156,6 +149,17 @@ namespace Proyecto_Final.Controllers
             return View(addUserTrainingViewModel);
         }
 
+        // GET: UserTraining/Edit/5
+        //[Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            ViewBag.UserFullName = GetUserFullName();
+            if (id == null || _context.UserTrainings == null) return NotFound();
 
+            var userTraining = await _context.UserTrainings.FindAsync(id);
+            if (userTraining == null) return NotFound();
+
+            return View(userTraining);
+        }
     }
 }
